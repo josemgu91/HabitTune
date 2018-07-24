859cf144f0fe915a10af717b47ff30b815742a8d
José Miguel García Urrutia <josemgu91@gmail.com>
2018-07-24 02:54:01 -0500

    Navigation View with options added. Basic style and colors. Menu names in english and spanish. Design support library added. Placeholder layout.

--- app/src/main/java/com/josemgu91/habittune/MainActivity.java
+++ app/src/main/java/com/josemgu91/habittune/MainActivity.java
@@ -19,14 +19,69 @@
 
 package com.josemgu91.habittune;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = setupActionBarDrawerToggle();
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        final NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private ActionBarDrawerToggle setupActionBarDrawerToggle() {
+        return new ActionBarDrawerToggle(this,
+                drawerLayout,
+                toolbar,
+                R.string.menu_navigation_open_drawer,
+                R.string.menu_navigation_close_drawer);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        drawerLayout.closeDrawers();
+        return true;
     }
 }
