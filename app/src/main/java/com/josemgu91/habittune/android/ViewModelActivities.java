87e92990f2e87e5f12e9a482673f20b7abf87f72
José Miguel García Urrutia <josemgu91@gmail.com>
2018-08-23 12:04:56 -0500

    ViewModelActivities created.

--- app/src/main/java/com/josemgu91/habittune/android/ViewModelActivities.java
+++ app/src/main/java/com/josemgu91/habittune/android/ViewModelActivities.java
@@ -19,24 +19,65 @@
 
 package com.josemgu91.habittune.android;
 
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.MutableLiveData;
+import android.support.annotation.NonNull;
 
+import com.josemgu91.habittune.adapter.ui.UseCaseExecutorWrapper;
+import com.josemgu91.habittune.adapter.ui.UseCaseOutputExecutorWrapper;
+import com.josemgu91.habittune.android.executors.DefaultThreadPoolExecutor;
+import com.josemgu91.habittune.android.executors.UiThreadExecutor;
 import com.josemgu91.habittune.domain.usecases.GetActivities;
+import com.josemgu91.habittune.domain.usecases.UseCase;
+import com.josemgu91.habittune.domain.usecases.UseCaseOutput;
 
 import java.util.List;
 
-public class ViewModelActivities extends ViewModel {
+public class ViewModelActivities extends AndroidViewModel {
 
-    private final GetActivities getActivities;
+    private final UseCase<Void> getActivities;
+
+    private final MutableLiveData<Boolean> isInProgress;
+    private final MutableLiveData<Boolean> hasError;
     private LiveData<List<GetActivities.Output>> activities;
 
-    public ViewModelActivities(final GetActivities getActivities){
-        this.getActivities = getActivities;
+    public ViewModelActivities(final Application application) {
+        super(application);
+        isInProgress = new MutableLiveData<>();
+        hasError = new MutableLiveData<>();
+        activities = new MutableLiveData<>();
+        this.getActivities = new UseCaseExecutorWrapper<>(
+                new DefaultThreadPoolExecutor(),
+                new GetActivities(new UseCaseOutputExecutorWrapper<>(
+                        new UseCaseOutput<LiveData<List<GetActivities.Output>>>() {
+                            @Override
+                            public void showResult(@NonNull LiveData<List<GetActivities.Output>> listLiveData) {
+                                isInProgress.setValue(false);
+                                hasError.setValue(false);
+                                activities = listLiveData;
+                            }
+
+                            @Override
+                            public void showInProgress() {
+                                isInProgress.setValue(true);
+                                hasError.setValue(false);
+                            }
+
+                            @Override
+                            public void showError() {
+                                isInProgress.setValue(false);
+                                hasError.setValue(true);
+                            }
+                        }, new UiThreadExecutor()),
+                        ((com.josemgu91.habittune.android.Application) application).getRoomRepository()
+                )
+        );
     }
 
-    public void init(){
-        //getActivities.execute();
+    public void fetchActivities() {
+        getActivities.execute(null);
     }
 
     public LiveData<List<GetActivities.Output>> getActivities() {
