068d9458eafcbe51b5d0f5c9776e523fc2aa17cc
José Miguel García Urrutia <josemgu91@gmail.com>
2018-09-25 12:10:41 -0500

    CreateRoutineEntry use case added.

--- app/src/main/java/com/josemgu91/habittune/domain/usecases/CreateRoutineEntry.java
+++ app/src/main/java/com/josemgu91/habittune/domain/usecases/CreateRoutineEntry.java
@@ -23,12 +23,15 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.josemgu91.habittune.domain.datagateways.RoutineDataGateway;
+import com.josemgu91.habittune.domain.entities.Activity;
+import com.josemgu91.habittune.domain.entities.RoutineEntry;
 import com.josemgu91.habittune.domain.usecases.common.AbstractUseCase;
 import com.josemgu91.habittune.domain.usecases.common.UseCaseOutput;
 
+import java.util.Objects;
 import java.util.concurrent.Executor;
 
-public class CreateRoutineEntry extends AbstractUseCase<CreateRoutineEntry.Input, CreateRoutineEntry.Output> {
+public class CreateRoutineEntry extends AbstractUseCase<CreateRoutineEntry.Input, Void> {
 
     private final RoutineDataGateway routineDataGateway;
 
@@ -38,15 +41,77 @@ public class CreateRoutineEntry extends AbstractUseCase<CreateRoutineEntry.Input
     }
 
     @Override
-    protected void executeUseCase(@Nullable Input input, @NonNull UseCaseOutput<Output> output) {
-
+    protected void executeUseCase(@Nullable Input input, @NonNull UseCaseOutput<Void> output) {
+        output.inProgress();
+        try {
+            final RoutineEntry routineEntry = routineDataGateway.createRoutineEntry(new RoutineEntry(
+                    "",
+                    new RoutineEntry.Day(input.day),
+                    new RoutineEntry.Time(input.startTime),
+                    new RoutineEntry.Time(input.endTime),
+                    new Activity(
+                            input.activityId,
+                            "",
+                            "",
+                            0,
+                            null
+                    )
+            ), input.routineId);
+            if (routineEntry != null) {
+                output.onSuccess(null);
+            } else {
+                output.onError();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            output.onError();
+        }
     }
 
     public static final class Input {
 
-    }
+        @NonNull
+        private final String routineId;
+        @NonNull
+        private final String activityId;
+        private final int day;
+        private final int startTime;
+        private final int endTime;
 
-    public static final class Output {
+        public Input(@NonNull String routineId, @NonNull String activityId, int day, int startTime, int endTime) {
+            this.routineId = routineId;
+            this.activityId = activityId;
+            this.day = day;
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
 
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Input input = (Input) o;
+            return day == input.day &&
+                    startTime == input.startTime &&
+                    endTime == input.endTime &&
+                    Objects.equals(routineId, input.routineId) &&
+                    Objects.equals(activityId, input.activityId);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(routineId, activityId, day, startTime, endTime);
+        }
+
+        @Override
+        public String toString() {
+            return "Input{" +
+                    "routineId='" + routineId + '\'' +
+                    ", activityId='" + activityId + '\'' +
+                    ", day=" + day +
+                    ", startTime=" + startTime +
+                    ", endTime=" + endTime +
+                    '}';
+        }
     }
 }
