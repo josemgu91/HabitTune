5482ff85daee04c936da47c0836aae02ce702a5c
José Miguel García Urrutia <josemgu91@gmail.com>
2018-10-03 16:32:21 -0500

    ViewModelRoutineUpdate implemented and added to the ViewModelFactory.

--- app/src/main/java/com/josemgu91/habittune/android/ui/routine_update/ViewModelRoutineUpdate.java
+++ app/src/main/java/com/josemgu91/habittune/android/ui/routine_update/ViewModelRoutineUpdate.java
@@ -19,7 +19,66 @@
 
 package com.josemgu91.habittune.android.ui.routine_update;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.josemgu91.habittune.android.ui.Response;
+import com.josemgu91.habittune.domain.usecases.UpdateRoutine;
+import com.josemgu91.habittune.domain.usecases.common.GetRoutine;
+import com.josemgu91.habittune.domain.usecases.common.UseCaseOutput;
 
 public class ViewModelRoutineUpdate extends ViewModel {
+
+    private final GetRoutine getRoutine;
+    private final UpdateRoutine updateRoutine;
+
+    private final MutableLiveData<Response<GetRoutine.Output, Void>> getRoutineResponse;
+
+    public ViewModelRoutineUpdate(GetRoutine getRoutine, UpdateRoutine updateRoutine) {
+        this.getRoutine = getRoutine;
+        this.updateRoutine = updateRoutine;
+        getRoutineResponse = new MutableLiveData<>();
+    }
+
+    public void fetchRoutine(@NonNull final String id) {
+        getRoutine.execute(new GetRoutine.Input(id), new UseCaseOutput<GetRoutine.Output>() {
+            @Override
+            public void onSuccess(@Nullable GetRoutine.Output output) {
+                getRoutineResponse.postValue(new Response<>(Response.Status.SUCCESS, output, null));
+            }
+
+            @Override
+            public void inProgress() {
+                getRoutineResponse.postValue(new Response<>(Response.Status.LOADING, null, null));
+            }
+
+            @Override
+            public void onError() {
+                getRoutineResponse.postValue(new Response<>(Response.Status.ERROR, null, null));
+            }
+        });
+    }
+
+    public void updateRoutine(@NonNull final String id, @NonNull final String name, @NonNull final String description, final int color) {
+        updateRoutine.execute(new UpdateRoutine.Input(id, name, description, color), new UseCaseOutput<Void>() {
+            @Override
+            public void onSuccess(@Nullable Void aVoid) {
+            }
+
+            @Override
+            public void inProgress() {
+            }
+
+            @Override
+            public void onError() {
+            }
+        });
+    }
+
+    public LiveData<Response<GetRoutine.Output, Void>> getGetRoutineResponse() {
+        return getRoutineResponse;
+    }
 }
