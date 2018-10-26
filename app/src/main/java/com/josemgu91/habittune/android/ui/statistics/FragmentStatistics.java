cb7d3c640912b7d837c912c103a179f66fa59a75
José Miguel García Urrutia <josemgu91@gmail.com>
2018-10-26 09:46:24 -0500

    CSV exportation menu added.

--- app/src/main/java/com/josemgu91/habittune/android/ui/statistics/FragmentStatistics.java
+++ app/src/main/java/com/josemgu91/habittune/android/ui/statistics/FragmentStatistics.java
@@ -25,6 +25,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -70,6 +73,25 @@ public class FragmentStatistics extends BaseFragment {
         final Bundle arguments = getArguments();
         activityId = arguments.getString(ARG_ACTIVITY_ID);
         dateFormatter = new DateFormatter();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_statistics, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.actionExportToCsv) {
+            return true;
+        }
+        return false;
     }
 
     @Override
